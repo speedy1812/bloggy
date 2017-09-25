a43b1454bf860af6506c443feb15c06b99bb5c32
Nathan Day <adayinukraine@gmail.com>
2017-09-25 16:20:32 +0300

    Create form and tweak spec

--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -5,7 +5,7 @@ feature "User creates new blog post", type: :feature do
     fill_in "Title", with: "Apple"
     fill_in "Text", with: "Apples are awesome!"
     
-    click_on "Create Article"
+    click_on "Save Article"
 
     expect(page).to have_css "p", text: 'Apples are awesome!'
   end
