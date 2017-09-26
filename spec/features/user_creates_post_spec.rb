e47d4828c9b30509194fa561b6dfe56aa7f8cc02
Nathan Day <adayinukraine@gmail.com>
2017-09-26 17:04:54 +0300

    Add test for invalid blog post title

--- spec/features/user_creates_post_spec.rb
+++ spec/features/user_creates_post_spec.rb
@@ -10,4 +10,16 @@ feature "User creates new blog post", type: :feature do
     expect(page).to have_css "p", text: 'Apple'
     expect(page).to have_css "p", text: 'Apples are awesome!'
   end
+
+  scenario "unsuccessfully" do
+    visit "articles/new" 
+    
+    fill_in "Title", with: "Ap"
+    fill_in "Text", with: "Apples are awesome!"
+    
+    click_on "Save Article"
+
+    expect(page).to have_css "h1", text: 'New Article'
+    
+  end
 end
