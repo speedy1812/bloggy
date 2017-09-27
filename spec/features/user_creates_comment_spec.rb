5e899a9685bb3b9c855c6dbcd48a2015ceaa11bd
Nathan Day <adayinukraine@gmail.com>
2017-09-27 15:23:31 +0300

    Create test for creating a comment

---
+++ spec/features/user_creates_comment_spec.rb
@@ -0,0 +1,18 @@
+feature "User creates comment", type: :feature do
+  scenario "successfully" do
+    visit "articles/new" 
+    
+    fill_in "Title", with: "Figs"
+    fill_in "Text", with: "Figs are fabulous!"
+    
+    click_on "Create Article"
+
+    fill_in "Commenter", with: "Fred"
+    fill_in "Body", with: "Flintstones are better."
+
+    click_on "Create Comment"
+
+    expect(page).to have_content("Fred")
+    expect(page).to have_content("Flintstones are better.")
+  end
+end
