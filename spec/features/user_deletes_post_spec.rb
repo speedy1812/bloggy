85828d0a7de9bd3874c1b80121d2a53fccbab6ef
Nathan Day <adayinukraine@gmail.com>
2017-09-27 13:49:45 +0300

    Add test for deleting posts

---
+++ spec/features/user_deletes_post_spec.rb
@@ -0,0 +1,15 @@
+feature "User deletes blog post", type: :feature do
+  scenario "successfully" do
+    visit "articles/new" 
+    
+    fill_in "Title", with: "Figs"
+    fill_in "Text", with: "Figs are fabulous!"
+    
+    click_on "Create Article"
+    click_on "Back"
+    click_on "Destroy"
+    click_on "OK"
+
+    expect(page).not_to have_content("Figs are fabulous!")
+  end
+end
